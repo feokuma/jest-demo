8011bc7810507be8900bf40718db246db4c457f7
Rodrigo Oliveira <rodrigocode4@gmail.com>
2022-07-08 11:15:13 -0300

    refator: removendo o mock da implementacao do axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
     const [tasks, setTasks] = useState<Task[]>();
